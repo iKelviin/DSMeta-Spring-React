7ef45aad0f1fea625498961a13c1191effbb1fc2
iKelviin <kelvinsantosdarocha@gmail.com>
2022-09-07 12:07:27 -0300

    SaleController
    Serviço de SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
 	
 	@Autowired
-	public SaleService service;
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -24,5 +28,10 @@ public class SaleController {
 			@RequestParam(value="maxDate", defaultValue = "")String maxDate,
 			Pageable pageable) {
 		return service.findSales(minDate,maxDate,pageable);
+	}
+	
+	@GetMapping("/notification")
+	public void notifySms() {
+		smsService.sendSms();
 	}
 }
